ff83c343d38f94ffa71f7d186057a30dc5ad57b8
Yeruva Srinivas Reddy <66551639+srinu-1@users.noreply.github.com>
2025-06-22 16:51:27 +0530

    Updated script.sh

--- script.sh
+++ script.sh
@@ -3,23 +3,23 @@ TAG=$1
 NAMESPACE=$2
 CHART_NAME=$3
 ACR_LOGIN_SERVER=$4
-CHART_DIR=$5  # This is the new parameter
+CHART_DIR=$5
 
 export KUBECONFIG=$HOME/.kube/config
 
-cd $CHART_DIR  # Use passed-in chart path
+cd "$CHART_DIR"
 
-helm ls -n $NAMESPACE | grep $CHART_NAME
-if [ $? -ne 0 ]; then
+if ! helm ls -n "$NAMESPACE" | grep -q "$CHART_NAME"; then
   echo "Installing Helm chart..."
-  helm install $CHART_NAME $CHART_NAME-$TAG.tgz \
-    --set image.repository=$ACR_LOGIN_SERVER/$CHART_NAME \
-    --set image.tag=$TAG \
-    --namespace $NAMESPACE --create-namespace
+  helm install "$CHART_NAME" "$CHART_NAME-$TAG.tgz" \
+    --set image.repository="$ACR_LOGIN_SERVER/$CHART_NAME" \
+    --set image.tag="$TAG" \
+    --namespace "$NAMESPACE" --create-namespace
 else
   echo "Upgrading Helm chart..."
-  helm upgrade $CHART_NAME $CHART_NAME-$TAG.tgz \
-    --set image.repository=$ACR_LOGIN_SERVER/$CHART_NAME \
-    --set image.tag=$TAG \
-    --namespace $NAMESPACE
+  helm upgrade "$CHART_NAME" "$CHART_NAME-$TAG.tgz" \
+    --set image.repository="$ACR_LOGIN_SERVER/$CHART_NAME" \
+    --set image.tag="$TAG" \
+    --namespace "$NAMESPACE"
 fi
+
