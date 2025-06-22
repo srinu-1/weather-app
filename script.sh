6b8a8caee1b5abf13a97f7f9f0df4cca2f164388
Yeruva Srinivas Reddy <66551639+srinu-1@users.noreply.github.com>
2025-06-22 17:32:03 +0530

    Updated script.sh

--- script.sh
+++ script.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
+
 TAG=$1
 NAMESPACE=$2
-CHART_NAME=$3
+IMAGE_NAME=$3
 ACR_LOGIN_SERVER=$4
 CHART_DIR=$5
 
 export KUBECONFIG=$HOME/.kube/config
 
-cd "$CHART_DIR"
+cd "$CHART_DIR" || exit 1
 
-if ! helm ls -n "$NAMESPACE" | grep -q "$CHART_NAME"; then
+CHART_PACKAGE="${NAMESPACE}-${TAG}.tgz"
+# The actual tgz file should be <chartName>-<version>.tgz
+# To make it generic:
+CHART_PACKAGE_FILE=$(ls *-"$TAG".tgz | head -1)
+
+if [ -z "$CHART_PACKAGE_FILE" ]; then
+  echo "Error: Helm chart package for version $TAG not found!"
+  exit 1
+fi
+
+if ! helm ls -n "$NAMESPACE" | grep -q "$IMAGE_NAME"; then
   echo "Installing Helm chart..."
-  helm install "$CHART_NAME" "$CHART_NAME-$TAG.tgz" \
-    --set image.repository="$ACR_LOGIN_SERVER/$CHART_NAME" \
+  helm install "$IMAGE_NAME" "$CHART_PACKAGE_FILE" \
+    --set image.repository="$ACR_LOGIN_SERVER/$IMAGE_NAME" \
     --set image.tag="$TAG" \
     --namespace "$NAMESPACE" --create-namespace
 else
   echo "Upgrading Helm chart..."
-  helm upgrade "$CHART_NAME" "$CHART_NAME-$TAG.tgz" \
-    --set image.repository="$ACR_LOGIN_SERVER/$CHART_NAME" \
+  helm upgrade "$IMAGE_NAME" "$CHART_PACKAGE_FILE" \
+    --set image.repository="$ACR_LOGIN_SERVER/$IMAGE_NAME" \
     --set image.tag="$TAG" \
     --namespace "$NAMESPACE"
 fi
