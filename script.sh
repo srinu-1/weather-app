7c23d33d847b737c4f710e021fd054f4cb32460d
Yeruva Srinivas Reddy <66551639+srinu-1@users.noreply.github.com>
2025-06-22 15:57:47 +0530

    Updated script.sh

--- script.sh
+++ script.sh
@@ -3,11 +3,11 @@ TAG=$1
 NAMESPACE=$2
 CHART_NAME=$3
 ACR_LOGIN_SERVER=$4
+CHART_DIR=$5  # This is the new parameter
 
 export KUBECONFIG=$HOME/.kube/config
 
-# Move to the chart directory inside the pipeline workspace
-cd $(System.DefaultWorkingDirectory)/_temp/charts  # Updated path based on artifact publish
+cd $CHART_DIR  # Use passed-in chart path
 
 helm ls -n $NAMESPACE | grep $CHART_NAME
 if [ $? -ne 0 ]; then
